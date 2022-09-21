117377ac238f6888edd246c8c32f2124aba02ca3
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-09-21 01:15:41 -0400

    fixed button to clear form on submit

--- src/Form.js
+++ src/Form.js
@@ -5,11 +5,18 @@ function Form({addItem}) {
   const [songTitle, setSongTitle] = useState("");
   const [artistName, setArtistName] = useState("");
 
+  function handleSubmit() {
+    addItem({ songTitle: songTitle, artistName: artistName});
+    setSongTitle("");
+    setArtistName("");
+    document.getElementById("Form").reset();
+  }
+
   return (
-    <form className="Form">
+    <form id="Form" className="Form">
       <input className="form-input" type="text" id="name" placeholder="Song Title" onChange={(e) => setSongTitle(e.target.value)}></input>
       <input className="form-input" type="text" id="name" placeholder="Artist" onChange={(e) => setArtistName(e.target.value)}></input>
-      <button className="form-button" type="button" onClick={() => addItem({ songTitle: songTitle, artistName: artistName})}>ADD</button>
+      <button className="form-button" type="button" onClick={() => handleSubmit()}>ADD</button>
     </form>
   );
 }
