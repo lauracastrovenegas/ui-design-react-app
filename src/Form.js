a1067cc32162c28e7bf391b5f7322df45341f29a
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-09-21 01:02:50 -0400

    Updated form to use handleAdd method

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
-function Form() {
+function Form({addItem}) {
+  const [songTitle, setSongTitle] = useState("");
+  const [artistName, setArtistName] = useState("");
+
   return (
     <form className="Form">
-      <input className="form-input" type="text" id="name" placeholder="Song Title"></input>
-      <input className="form-input" type="text" id="name" placeholder="Artist"></input>
-      <button className="form-button">ADD</button>
+      <input className="form-input" type="text" id="name" placeholder="Song Title" onChange={(e) => setSongTitle(e.target.value)}></input>
+      <input className="form-input" type="text" id="name" placeholder="Artist" onChange={(e) => setArtistName(e.target.value)}></input>
+      <button className="form-button" type="button" onClick={() => addItem({ songTitle: songTitle, artistName: artistName})}>ADD</button>
     </form>
   );
 }
